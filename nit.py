f067d02aba190c36673afdc2e9958121ab61d70c
mactanxin <naruto97d@gmail.com>
2011-03-11 11:22:26 +0800

    切分为show_list()和show_page()方法

--- nit.py
+++ nit.py
@@ -6,34 +6,34 @@ from urllib2 import build_opener
 import urllib2
 import random
 
-def show(fid,page_type="fid"):
+def show_list(fid):
     opener = build_opener()
-    if page_type == "fid":
-        a_link="http://bbs.ngacn.cc/thread.php?fid=%s" %fid
-        try:
-            page=opener.open(a_link).read().decode('gbk').encode('utf-8')
-            links = re.findall(r"<a href=.read\.php\?tid=(\d{7})\&\_fp=1.\s+id.*>(.*?)</a>",page)
-            for i in links:
-                print "%s|%s" %(i[0],i[1])
-        except ValueError:
-            print " value error"
-            raise
-        except urllib2.HTTPError:
-            # print ""  
-            raise     
-                    
-    else :
-        a_link='http://bbs.ngacn.cc/read.php?tid=%s' %fid
-        try:
-            page=opener.open(a_link).read().decode('gbk').encode('utf-8') 
-            contents = re.findall(r'<span.id=.postcontent.*?</span>',page)
-            for i in contents:
-                i = i.replace('<span id=\'',"").replace("</span>","").replace("\' class='postcontent ubbcode\'","").replace("<br/>","|")
-                k = re.sub(r'\[.*\]','',i)       
-                print k
-        except ValueError:
-            print "error"
-            # raise       
+    a_link="http://bbs.ngacn.cc/thread.php?fid=%s" %fid
+    try:
+        page=opener.open(a_link).read().decode('gbk').encode('utf-8')
+        links = re.findall(r"<a href=.read\.php\?tid=(\d{7})\&\_fp=1.\s+id.*>(.*?)</a>",page)
+        for i in links:
+            print "%s|%s" %(i[0],i[1])
+    except ValueError:
+        print " value error"
+        raise
+    except urllib2.HTTPError:
+        # print ""  
+        raise     
+                
+def show_page(fid):
+    opener = build_opener()
+    a_link='http://bbs.ngacn.cc/read.php?tid=%s' %fid
+    try:
+        page=opener.open(a_link).read().decode('gbk').encode('utf-8') 
+        contents = re.findall(r'<span.id=.postcontent.*?</span>',page)
+        for i in contents:
+            i = i.replace('<span id=\'',"").replace("</span>","").replace("\' class='postcontent ubbcode\'","").replace("<br/>","|")
+            k = re.sub(r'\[.*\]','',i)       
+            print k
+    except ValueError:
+        print "error"
+        # raise       
 def preview_page():
     pass
 
